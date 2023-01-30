81a8f6a63a9bcb9a6a3472257fc0ce487f4fc459
DanielBenjaminLopez <benja248myn@gmail.com>
2023-01-30 18:57:08 -0300

    agregoBuscador

--- PORTAFOLIO/src/app/app.component.ts
+++ PORTAFOLIO/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChange,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'PORTAFOLIO';
+  title = 'Portafolio';
+
+  cities = ['Corrientes', 'Salta', 'Buenos Aires', 'Misiones', 'Santa Fe'];
+  portada =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIgeYD_L_QfrfKoMBesOrRlMXkVZPmn9R0mg&usqp=CAU';
+  selection!: string;
+  nombre = 'ejemplo';
+
+  onCityClicked(city: string): void {
+    console.log('City ->', city);
+    this.selection = city;
+  }
+
+  onClear(): void {
+    this.selection = '';
+  }
+
+  addNewCity(city: string): void {
+    this.cities.push(city);
+  }
 }
---
+++ PORTAFOLIO/src/app/button/button.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent implements OnChanges, OnInit, OnDestroy {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes ->', changes);
+  }
+  ngOnInit(): void {
+    console.log('OnInit');
+  }
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
+  @Input() color!: string;
+  @Input() label!: string;
+}
